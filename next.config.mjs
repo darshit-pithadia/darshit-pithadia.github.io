a84f8b9165221ec11cc103661ba53b417bdcee85
Darshit Pithadia <darshit.pithadia0503@gmail.com>
2025-06-08 18:50:19 +0530

    Setup next.config.mjs for static export and GitHub Pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,20 @@
+// next.config.mjs
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
+  trailingSlash: true,
+  assetPrefix: './',
+  basePath: '',
+  images: {
+    unoptimized: true,
+  },
   eslint: {
     ignoreDuringBuilds: true,
   },
   typescript: {
     ignoreBuildErrors: true,
-  },
-  images: {
-    unoptimized: true,
-  },
-}
+  }
+};
 
-export default nextConfig
+export default nextConfig;
